32ae52769741457bbad1b084ad58484561b49a29
Peter Forsgren Christensen <peter.f.chris@gmail.com>
2019-04-30 15:08:47 -0600

    Completed app-5

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -1,18 +1,13 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Image from './Image'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Image myImage={'https://http:pm1.narvii.com/6488/ad82146543e9ec9e184ec360c6522075652300aa_hq.jpg'}/>
       </div>
     );
   }
---
+++ app-5/src/Image.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class Image extends Component{
+    render(){
+        return(
+            <div>
+                <img src={this.props.url} />
+                <caption>Error 509</caption>
+            </div>
+        )
+    }
+
+
+}
